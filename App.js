dbf231b115045cb6373b99dd574fedf84b7a122b
Sewon Kim <sewonkim@Sewonui-MacBookAir.local>
2021-10-28 16:12:38 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,102 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native'
+
+import * as firebase from 'firebase'
+
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './redux/reducers'
+import thunk from 'redux-thunk'
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2DGwAQGwxKOP-q_Z6nQYQ0gnEtQIRx4s",
+  authDomain: "instagram-70282.firebaseapp.com",
+  projectId: "instagram-70282",
+  storageBucket: "instagram-70282.appspot.com",
+  messagingSenderId: "505416316829",
+  appId: "1:505416316829:web:6a9f82894baed296e8e174",
+  measurementId: "G-NDYKK5D4JM"
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen from './components/auth/Register'
+import LoginScreen from './components/auth/Login'
+import MainScreen from './components/Main'
+import AddScreen from './components/main/Add'
+import SaveScreen from './components/main/Save'
+
+
+const Stack = createStackNavigator();
+
+
+export class App extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text>Loading</Text>
+        </View>
+      )
+    }
+
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+
+    return (
+      <Provider store={store}>
+        <NavigationContainer >
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen name="Main" component={MainScreen} />
+            <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation}/>
+            <Stack.Screen name="Save" component={SaveScreen} navigation={this.props.navigation}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    )
+  }
+}
+
+export default App
